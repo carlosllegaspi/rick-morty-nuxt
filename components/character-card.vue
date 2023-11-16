78810c40958abac3164bed348ebf87cb14429359
Carlos Lopez Legaspi <carlos.lopez.legaspi@melia.com>
2023-11-16 12:44:59 +0100

    center image

--- components/character-card.vue
+++ components/character-card.vue
@@ -21,7 +21,7 @@ const link = `/character/${character.id}`
 
 <template>
     <NuxtLink :to="link">
-        <div class="p-3 hover:p-5" :class="bgColorMap[character.status]">
+        <div class="p-3 hover:p-5 flex flex-col justify-center mt-5 md:mt-0" :class="bgColorMap[character.status]">
             <img :src="character.image" :alt="character.name" />
             <h2 class="text-xl font-semibold pt-2">{{ character.name }}</h2>
             <p class="pt-2 text-sm">Última ubicación conocida: 
